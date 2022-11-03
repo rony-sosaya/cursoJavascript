0f35c784a27106f3336197921fe55547f91c86cf
rony <ronysosaya@gmail.com>
2022-11-02 22:53:54 -0500

    Estructura_Ciclo}

---
+++ 6.EstructuraCiclo/ejercicio.js
@@ -0,0 +1,22 @@
+// CICLO FOR
+
+for ( var i = 0; i <= 10; i++){
+    console.log( i + "Hola!");
+}
+
+// CICLO WHILE
+
+var o = 0;
+while (o < 30) {
+    console.log(o);
+    o ++;
+}
+
+// CICLO DO-WHILE
+
+var abecedario = 27
+do {
+    console.log("Contador en " + abecedario);
+    abecedario ++;
+}
+while (abecedario < 28);
---
+++ 6.EstructuraCiclo/index.js
@@ -0,0 +1,11 @@
+//ESTRUCTURA CICLO
+// CICLO FOR
+// Este tipo de ciclo es determinado por la cantidad de datos que contiene un objeto
+// Los ejemplos pueden ser un arreglo, una matriz, etc.
+// CICLO WHILE
+// Estos ciclos se ejecutan siempre y cuando una condición específica sea verdadero
+// (True).
+// CICLO DO-WHILE
+// Es la variante del ciclo While, este tipo de ciclo primero ejecuta el bloqué de código
+// sin importar si la condición es verdadera, en seguida repetirá siempre y cuando la condición
+// se cumpla
